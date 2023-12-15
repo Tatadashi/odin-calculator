73f0f601d6051fea7a0e3a1620cc359ca0daf180
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2023-12-15 02:16:05 -0800

    Add new fn to use operate() & update old fns
    Change operate to work with new symbols for multiply/divide. Rename changeDisplay() for clarification and have it use new fn checkButton() instead. Rename display() for clarification and parameter to be a string instead of node. Add 3 new global bool to help with storing values for checkButton() and change default values of other global var. checkButton() only updates display and stores values if button clicked in correct order (only 1 operator allowed, equal only works when there is 2 numbers and an operator)

--- script.js
+++ script.js
@@ -61,10 +61,10 @@ function operate(firstNum, operator, secondNum) {
         case `-`:
             answer = subtract(firstNum, secondNum);
             break; 
-        case `*`:
+        case `x`:
             answer = multiply(firstNum, secondNum);
             break;
-        case `/`:
+        case `÷`:
             answer = divide(firstNum, secondNum);
             break;
         default:
@@ -74,30 +74,67 @@ function operate(firstNum, operator, secondNum) {
     return answer;
 }
 
-//changes display to reflect one specific button click
-function display(button) {
+//changes display to add new text
+function addToDisplay(text) {
     let currentText = document.querySelector(`.display`).textContent;
-
-    let buttonText = button.textContent;
-    
-    let newText = currentText + buttonText;
+    let newText = currentText + text;
 
     document.querySelector(`.display`).textContent = newText;
 }
 
-//changes display when any button is clicked
-function changeDisplay() {
+//checks which type of button is clicked
+function checkButton(button) {
+    let displayText = ``;
+    if (button.textContent == `=`) {
+        if (isOperable) {
+            let answer = operate(Number(firstNum), operator, Number(secondNum));
+            document.querySelector(`.display`).textContent = answer;
+            firstNum = answer;
+
+            //reset to default values
+            operator = `N/A`;
+            secondNum = ``;
+            isFirstNum = true;
+            hasOperator = false;
+            isOperable = false;
+        }
+    } else if (button.classList.contains(`operator`)) {
+        if (!hasOperator) {
+            operator = button.textContent;
+            isFirstNum = false;
+            hasOperator = true;
+
+            displayText = operator;
+            addToDisplay(displayText);
+        }
+    } else {
+        if (isFirstNum) {
+            firstNum += button.textContent;
+        } else {
+            secondNum += button.textContent;
+            isOperable = true;
+        }
+
+        displayText = button.textContent;
+        addToDisplay(displayText);
+    }
+}
+
+//adds click event for every button
+function addButtonClickEvents() {
     let buttons = document.querySelectorAll(`.pad`);
     buttons.forEach((button) => {
         button.addEventListener(`click`, () => {
-            display(button);
+            checkButton(button);
         });
     });
 }
 
-let firstNum = 10;
-let secondNum = 2;
-let operator = `/`;
+let firstNum = ``;
+let operator = `N/A`;
+let secondNum = ``;
+let isFirstNum = true;
+let hasOperator = false;
+let isOperable = false;
 
-console.log(operate(firstNum, operator, secondNum));
-changeDisplay();
+addButtonClickEvents();
